ea6bc0679b1d6d4940ecd4d704f599069e7e201a
Corey Rodems <coreyrodems@gmail.com>
2018-02-21 01:18:14 -0800

    removed serve-favicon package

--- server.js
+++ server.js
@@ -116,8 +116,6 @@ const partialsDir = __dirname + '/views/partials';
 // *****************************************************************************
 // * *** Express app setup
 // =============================================================================
-// = uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
